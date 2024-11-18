ca38e2ad3ef789b25e2bfb4dca8ffd42a9b292e6
dotsonph <dotsonph01@gmail.com>
2024-11-18 15:08:54 -0500

    Modified page.tsx - Changed window sizes to accomodate smaller screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 	return (
     <div className="font-mono">
 		<div className = "bg-gray-700 text-gray-400">
-			<span className="md:w-full inline-block text-xl text-right font-bold">
+			<span className="w-full inline-block text-xl text-right font-bold text-gray-300">
 				- o <span className="text-red-600">x&nbsp;</span>
 			</span>
 			<h1 className="font-bold text-center pb-10 text-xl">dotson@Yoga-C-930:~/Documents/portfolio</h1>
@@ -15,18 +15,18 @@ export default function Home() {
 		</div>
 
 		<Create_Next/>
-		
-		<div className="flex flex-row">
-			<span className="basis-1/3 pt-8 pl-60">
-				<Terminal_ID/>
-			</span>
-			<span className="basis-1/3 flex flex-col pb-40">
-				<Terminal_Intro/>
-			</span>
+		<br/>
 
-			<span className ="basis-1/3 pl-24 pt-10">
+		<div className="grid grid-cols-8 grid-rows-2 gap-4">
+			<div className="row-start-1 row-span-2 col-start-2 col-span-3 row-span-2">
+				<Terminal_Intro/>
+			</div>
+			<div className="col-start-5 col-span-4 row-span-1 row-start-1">
+				<Terminal_ID/>
+			</div>
+			<div className="col-start-5 col-span-4 row-span-1 row-start-2">
 				<Terminal_Contact/>
-			</span>
+			</div>
 		</div>
 
 		<Create_Proj/>
@@ -38,35 +38,33 @@ export default function Home() {
 function Senior_Design() {
 	return (
 		<div className="pt-10 pl-40">
-			<div className="md:w-2/5 border border-gray-600 text-gray-300 bg-gray-700 text-xl">
-				<span className="md:w-full inline-block text-right font-bold">
+			<div className="md:w-2/5 border border-gray-600 text-gray-300 bg-gray-700 text-md">
+				<span className="md:w-full inline-block text-right font-bold text-xl">
 					- o <span className="text-red-600">x&nbsp;</span>
 				</span>
 
-			<p className="md:w-full pb-0.5 text-center">
+				<p className="text-xl md:w-full pb-0.5 text-center">
 				RF Antenna Feed Development &lt;proj&gt;
-			</p>
-			<div className="pb-10 bg-black px-2 pt-1">
-				<p className="text-sm text-gray-400">
-					/*This project is one I hold close to my heart. I enjoyed the countless nights spent studying, simulating, constructing, and 
-					presenting with my beloved team and supervisor. The doors it opened for me is indescribable.*/
 				</p>
-				<br/>
-				<p>
-					Originally a Senior Design project. Later competed in the 2024 Annual SSPI Mid-Atlantic Regional Chapter's 
-					Aerospace Science & Engineering Competition.
-				</p>
-				<br/>
-				<p>
-					Our task being to construct two antenna feeds of varying frequency bands to place within a 9m Cassegrain Dish. An entire semester was 
-					dedicated to simulations and research.
-				</p>
-				<br/>
-				<p>
-					The next semester consisted of soldering, construction, and testing. Further tasks included compiling journal entries, rehearsing, presenting, extensive technical 
-					writing, and awaiting the long anticipated competition.
-				</p>
-			</div>
+
+				<div className="pb-10 bg-black px-2 pt-1">
+					<p>
+						Originally a Senior Design project and later adapted to compete in the 2024 SSPI Mid-Atlantic Regional Chapter's 
+						Aerospace Science & Engineering Competition.
+					</p>
+					<br/>
+					<p>
+						Our task being to construct two antenna feeds (L/X Band) for the GMU SATCOM Club's 9m Cassegrain Dish.
+					<p>
+					<br/>
+						An entire semester was dedicated to simulations and research using ANSYS HFSS/CST Studio.
+					</p>
+					</p>
+					<br/>
+					<p>
+						The next semester consisted of construction, and testing. Further tasks included compiling journal entries, rehearsing, presenting, drafting technical reports, and awaiting the long anticipated competition.
+					</p>
+				</div>
 			</div>		
 		</div>
 	)
@@ -79,23 +77,24 @@ function Create_Proj() {
 			<div className="flex flex-row gap-8">
 				<span className="pl-2">
 					<p>Backup_Files</p>
-					<p>GMU</p>
-				</span>
-				<span>
-					<p>Academic Transcript.pdf</p>
-					<p>Dotson_Pham_Resume.odt</p>
-				</span>
-				<span>
-					<p>Projects</p>
 					<p>Screenshots</p>
 				</span>
 				<span>
-					ECE_Stuff
+					<p>Transcripts</p>
+					<p>Hidden_Goods</p>
+				</span>
+				<span>
+					<p>Archives</p>
+					<p>GMU</p>
+				</span>
+				<span>
+					<p>Lab_Reports</p>
 				</span>
 			</div>
 
-			<p><Name /><span className="text-blue-300">/Documents</span>$ cd Projects</p>
-			<p><Name /><span className="text-blue-300">/Documents/Projects</span>$</p>
+			<p><Name /><span className="text-blue-300">/Documents</span>$ cd GMU/Projects</p>
+			<p><Name /><span className="text-blue-300">/Documents/GMU/Projects</span>$ gcc senior_design.c -o exec</p>
+			<p><Name /><span className="text-blue-300">/Documents/GMU/Projects</span>$ ./exec</p>
 		</div>
 	)
 }
@@ -109,37 +108,6 @@ function Create_Next() {
 			<span className="text-blue-300">/Documents</span>
 			<span className="pr-2">$ npx create-next-app@latest portfolio</span>
 			<br/>
-
-			<span className="text-lg text-green-400">&nbsp;&#x2713; </span>
-			<span className="font-bold">
-				Would you like to use <span className="text-violet-300">TypeScript</span>? <span className="font-normal"> ... No / <span className="underline text-cyan-300">Yes</span></span>
-			</span><br/>
-
-			<span className="text-lg text-green-400">&nbsp;&#x2713; </span>
-			<span className="font-bold">
-				Would you like to use <span className="text-violet-300">ESLint</span>? <span className="font-normal"> ... No / <span className="underline text-cyan-300">Yes</span></span>
-			</span><br/>
-
-			<span className="text-lg text-green-400">&nbsp;&#x2713; </span>
-			<span className="font-bold">
-				Would you like to use <span className="text-violet-300">Tailwind CSS</span>? <span className="font-normal"> ... No / <span className="underline text-cyan-300">Yes</span></span>
-			</span><br/>
-
-			<span className="text-lg text-green-400">&nbsp;&#x2713; </span>
-			<span className="font-bold">
-				Would you like to use <span className="text-violet-300">`src/` directory</span>? <span className="font-normal"> ... No / <span className="underline text-cyan-300">Yes</span></span>
-			</span><br/>
-
-			<span className="text-lg text-green-400">&nbsp;&#x2713; </span>
-			<span className="font-bold">
-				Would you like to use <span className="text-violet-300">App Router</span>? (recommended)<span className="font-normal"> ... No / <span className="underline text-cyan-300">Yes</span></span>
-			</span><br/>
-
-			<span className="text-lg text-green-400">&nbsp;&#x2713; </span>
-			<span className="font-bold">
-				Would you like to customize the default <span className="text-violet-300">import alias</span> (@/*)? <span className="font-normal"> ... No / <span className="underline text-cyan-300">Yes</span></span>
-			</span><br/>
-
 			<span>
 				&nbsp;Creating a new Next.js app in <span className="text-green-400">/Users/dotson/Documents/portfolio</span>.
 			</span>
@@ -160,36 +128,31 @@ function Name () {
 function Top_Row() {
 	return (
 		<div className="text-lg bg-gray-600 flex space-x-4">
-			<span className="pl-3">File</span>
-			<span>Edit</span>
-			<span>View</span>
-			<span>Search</span>
-			<span>Terminal</span>
-			<span>Help</span>
+			<span className="pl-3">Intro</span>
+			<span>Projects</span>
+			<span>Experience</span>
+			<span>Resume</span>
+			<span>Contact</span>
 		</div>		
 	)
 }
 
 function Terminal_Contact() {
 	return (
-		<div className="border border-gray-600 text-xl inline-block bg-gray-700 text-gray-300">
-			<span className="md:w-full inline-block text-right font-bold">
+		<div className="border border-gray-600 inline-block bg-gray-700 text-gray-300">
+			<span className="md:w-full inline-block text-xl text-right font-bold">
 				- o <span className="text-red-600">x&nbsp;</span>
 			</span>
 
-			<p className="text-center">
+			<p className="text-center text-xl">
 				find_me.txt
 			</p>
-			<div className="pb-40 bg-black pt-1 pr-20">
+			<div className="pb-20 bg-black pt-1 pr-10 text-md">
 
 				<div>
 					<span className = "text-gray-500">-- </span>
 					<span className = "text-blue-300">@Email: </span>
 					<span>dotsonph01@gmail.com</span>
-					<br/>
-					<span className = "text-gray-500">-- </span>
-					<span className = "text-blue-300">@Mobile: </span>
-					<span>703-677-1143</span>
 					<br/>
 				</div>
 
@@ -221,15 +184,15 @@ function Terminal_Contact() {
 
 function Terminal_ID() {
 	return (
-		<div className="border border-gray-600 inline-block bg-gray-700 text-gray-300 text-xl">
-			<span className="md:w-full inline-block text-right font-bold">
-				- o <span className="text-red-600">x&nbsp;</span>
+		<div className="border border-gray-600 inline-block bg-gray-700 text-gray-300">
+			<span className="text-xl md:w-full inline-block text-right font-bold">
+				- o <span className="text-xl text-red-600">x&nbsp;</span>
 			</span>
 
-			<p className="text-center">
+			<p className="text-center text-xl">
 				who_am_i.txt 
 			</p>
-			<div className="pb-40 bg-black pt-1">
+			<div className="pb-10 bg-black pt-1 text-md">
 
 				<div>
 					<span className = "text-gray-500">-- </span>
@@ -291,15 +254,15 @@ function Terminal_ID() {
 
 function Terminal_Intro() {
 	return (
-		<div className="border border-gray-600 text-gray-300 bg-gray-700 text-xl">
-				<span className="md:w-full inline-block text-right font-bold">
+		<div className="border border-gray-600 text-gray-300 bg-gray-700 w-full">
+				<span className="text-xl md:w-full inline-block text-right font-bold">
 					- o <span className="text-red-600">x&nbsp;</span>
 				</span>
 
-			<p className="md:w-full pb-0.5 text-center">
+			<p className="md:w-full pb-0.5 text-center text-xl">
 				introduction.png
 			</p>
-			<p className="pb-10 bg-black px-2 pt-1">
+			<p className="pb-10 bg-black px-2 pt-1 text-md">
 				Hello! I'm Dotson and I recently graduated with a 
 				<span className="text-yellow-300"> B.S in Computer Engineering </span>
 				with a focus on
@@ -308,7 +271,7 @@ function Terminal_Intro() {
 				I am currently seeking professional opportunities where I can advance my career. Feel free to reach out if you find me a good fit! I am confident I can be a great asset to any team.
 				</span>
 			</p>
-			<div className="pb-10 bg-black pl-40">
+			<div className="pb-10 bg-black flex justify-center">
 				<Image className="border-2 border-dotted hover:border-solid border-yellow-300" src="/studious.png" width={350} height={400} alt = "photo of me trying to be productive"/>
 			</div>
 		</div>
